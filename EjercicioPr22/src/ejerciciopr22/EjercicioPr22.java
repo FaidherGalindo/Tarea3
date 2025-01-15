03afcaf5d9bae30224f36176e3a404258f30225e
FaidherGalindo <fgalindo@unal.edu.co>
2025-01-14 21:05:03 -0500

    Add files via upload

---
+++ EjercicioPr22/src/ejerciciopr22/EjercicioPr22.java
@@ -0,0 +1,8 @@
+package ejerciciopr22;
+public class EjercicioPr22 {
+
+    public static void main(String[] args) {
+        VentanaPrincipal ventana = new VentanaPrincipal();
+        ventana.setVisible(true);  
+    }
+}
---
+++ EjercicioPr22/src/ejerciciopr22/Empleado.java
@@ -0,0 +1,26 @@
+package ejerciciopr22;
+
+
+public class Empleado {
+    
+    public static double calcular_salario_mensual(double salario_basico_hora, double numero_horas_trabajadas){
+    double salario_mensual;
+    
+    salario_mensual=salario_basico_hora*numero_horas_trabajadas;
+    return salario_mensual;
+}
+  
+    public static String set_Nombre(String nombre){
+    return nombre;
+    
+}  
+    
+
+    public static String limpiar_Campos(){
+    
+    return "";
+    
+}  
+    
+    
+}
