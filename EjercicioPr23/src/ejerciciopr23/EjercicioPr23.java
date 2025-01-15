1f756239dfc1c335732ed9b69f87474c85e78cbd
FaidherGalindo <fgalindo@unal.edu.co>
2025-01-14 21:05:28 -0500

    Add files via upload

---
+++ EjercicioPr23/src/ejerciciopr23/EjercicioPr23.java
@@ -0,0 +1,9 @@
+package ejerciciopr23;
+public class EjercicioPr23 {
+
+    public static void main(String[] args) {
+        VentanaPrincipal ventana = new VentanaPrincipal();
+        ventana.setVisible(true);  
+    }
+    
+}
---
+++ EjercicioPr23/src/ejerciciopr23/Soluciones.java
@@ -0,0 +1,21 @@
+package ejerciciopr23;
+
+
+public class Soluciones {
+    double a,b,c;
+    
+public static double calcular_solucion1(double a,double b, double c){
+
+    return (((-1)*b) + Math.sqrt(Math.pow(b,2)-4*a*c))/(2*a);
+}
+
+public static double calcular_solucion2(double a,double b, double c){
+  
+    return (((-1)*b) - Math.sqrt(Math.pow(b,2)-4*a*c))/(2*a);
+}
+    
+public static String limpiar_Campos(){
+    return "";
+}    
+    
+}
