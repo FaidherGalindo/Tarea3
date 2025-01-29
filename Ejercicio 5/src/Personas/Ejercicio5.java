0f988210a27c75b7090a3b9ce0967adec0eac44d
FaidherGalindo <fgalindo@unal.edu.co>
2025-01-28 22:26:06 -0500

    Add files via upload

---
+++ Ejercicio 5/src/Personas/Ejercicio5.java
@@ -0,0 +1,11 @@
+package Personas;
+
+
+public class Ejercicio5 {
+   public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal;
+        miVentanaPrincipal= new VentanaPrincipal();
+        miVentanaPrincipal.setVisible(true);
+        }
+    
+}
