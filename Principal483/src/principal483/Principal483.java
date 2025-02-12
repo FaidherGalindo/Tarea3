a1eef6048a34c9ea64bb2d6cb8e7da3fab246958
FaidherGalindo <fgalindo@unal.edu.co>
2025-02-11 22:15:08 -0500

    Add files via upload

---
+++ Principal483/src/principal483/Principal483.java
@@ -0,0 +1,10 @@
+package principal483;
+
+public class Principal483 {
+    
+    public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal;
+        miVentanaPrincipal= new VentanaPrincipal(); 
+        miVentanaPrincipal.setVisible(true); 
+    }
+}
