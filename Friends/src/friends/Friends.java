faea60fe6d247d22566e0103aa4c730297e7eb19
FaidherGalindo <fgalindo@unal.edu.co>
2025-03-04 08:58:01 -0500

    Add files via upload

---
+++ Friends/src/friends/Friends.java
@@ -0,0 +1,15 @@
+package friends;
+
+
+public class Friends {
+
+    public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal;
+        
+        miVentanaPrincipal = new VentanaPrincipal();
+        
+        miVentanaPrincipal.setVisible(true);
+
+    }
+    
+}
